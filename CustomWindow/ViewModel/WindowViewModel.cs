48c549da059a37cc1148da647af4c9d899846620
Mental Refinery <mentalrefinery@gmail.com>
2020-03-11 17:35:44 +0200

    Empty CustomWindow Chrome Import

---
+++ CustomWindow/ViewModel/WindowViewModel.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Input;
+
+
+namespace CustomWindow.ViewModel
+{
+    /// <summary>
+    /// View model for the Custom Flat Window
+    /// </summary>
+     public class WindowViewModel: BaseViewModel
+     {
+        #region Primate Member
+        private Window mWindow;
+
+        /// <summary>
+        /// The margin around the window to allow for a drop shadow.
+        /// </summary>
+        private int mOuterMarginSize = 20;
+        /// <summary>
+        /// The radius of the edges of the window.
+        /// </summary>
+        private int mWindowRadius = 10;
+        #endregion
+        #region Public Properties
+
+        /// <summary>
+        /// The smallest window width allowed.
+        /// </summary>
+        public double WindowMinimumWidth { get; set; } = 400;
+        /// <summary>
+        /// The smallest window height allowed.
+        /// </summary>
+        public double WindowMinimumHeight { get; set; } = 400;
+
+
+        /// <summary>
+        /// The size of the resize border around the window
+        /// </summary>
+        public int ResizeBorder { get; set; } = 6;
+        /// <summary>
+        /// The the padding of the inner content of the main window
+        /// </summary>
+        public Thickness InnerContentPaddingThickness { get { return new Thickness(ResizeBorder); } }
+        /// <summary>
+        /// Size of the resize border around the window, taking into account the outer margin.
+        /// </summary>
+        public Thickness ResizeBorderThickness { get { return new Thickness(ResizeBorder + OuterMarginSize); } }
+
+        /// <summary>
+        /// The margin around the window to allow for a drop shadow.
+        /// </summary>
+        public int OuterMarginSize
+        {
+            get
+            {
+                return mWindow.WindowState == WindowState.Maximized ? 0 : mOuterMarginSize;
+            }
+            set
+            {
+                mOuterMarginSize = value;
+            }
+        }
+
+        /// <summary>
+        /// The margin around the window to allow for a drop shadow.
+        /// </summary>
+        public Thickness OuterMarginSizeThickness { get { return new Thickness(OuterMarginSize); } }
+       
+
+        /// <summary>
+        /// The radius of the edges of the window.
+        /// </summary>
+        public int WindowRadius
+        {
+            get
+            {
+                return mWindow.WindowState == WindowState.Maximized ? 0 : mWindowRadius;
+            }
+            set
+            {
+                mWindowRadius = value;
+            }
+        }
+        /// <summary>
+        /// The radius of the edges of the window.
+        /// </summary>
+        public CornerRadius WindowCornerRadius { get { return new CornerRadius(WindowRadius); } }
+
+        /// <summary>
+        /// The height of the Title Bar / Caption of the window
+        /// </summary>
+        public int TitleHeight { get; set; } = 42;
+        /// <summary>
+        /// The height of the Title Bar / Caption of the window
+        /// </summary>
+        public GridLength TitleHeightGridLength { get { return new GridLength(TitleHeight + ResizeBorder); } }
+        #endregion
+        #region Commands
+
+        /// <summary>
+        /// The command to minimize the window.
+        /// </summary>
+        public ICommand MinimizeCommand { get; set; }
+
+        /// <summary>
+        /// The command to maximize the window.
+        /// </summary>
+        public ICommand MaximizeCommand { get; set; }
+
+        /// <summary>
+        /// The command to close the window.
+        /// </summary>
+        public ICommand CloseCommand { get; set; }
+
+        /// <summary>
+        /// The command to show the system menu of the window.
+        /// </summary>
+        public ICommand MenuCommand { get; set; }
+
+        #endregion
+        #region Constructor
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public WindowViewModel(Window window)
+        {
+            mWindow = window;
+
+            //Listen out for the window resizing
+#pragma warning disable CA1062 // Validate arguments of public methods
+            mWindow.StateChanged += (sender, e) =>
+#pragma warning restore CA1062 // Validate arguments of public methods
+            {
+                //Fire off events for all properties that are affected by a resize
+                OnPropertyChanged(nameof(ResizeBorderThickness));
+                OnPropertyChanged(nameof(OuterMarginSize));
+                OnPropertyChanged(nameof(OuterMarginSizeThickness));
+                OnPropertyChanged(nameof(WindowRadius));
+                OnPropertyChanged(nameof(WindowCornerRadius));
+            };
+
+            //Create Commands
+            MinimizeCommand = new RelayCommand(() => mWindow.WindowState = WindowState.Minimized);
+            MaximizeCommand = new RelayCommand(() => mWindow.WindowState ^= WindowState.Maximized);
+            CloseCommand = new RelayCommand(() => mWindow.Close());
+            MenuCommand = new RelayCommand(() => SystemCommands.ShowSystemMenu(mWindow, GetMousePosition()));
+
+            //Fix Window Resize Issue
+            var resizer = new WindowResizer(mWindow);
+        }
+
+
+        #endregion
+        #region Private helpers
+        /// <summary>
+        /// Gets the current mouse position on the screen
+        /// </summary>
+        /// <returns></returns>
+        private Point GetMousePosition()
+        {
+            // Position of the mouse relative to the window
+            var position = Mouse.GetPosition(mWindow);
+
+            // Add the window position so its a "ToScreen"
+            return new Point(position.X + mWindow.Left, position.Y + mWindow.Top);
+        }
+
+        #endregion
+    }
+}
